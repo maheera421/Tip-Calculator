a8aa3ebdfdef91677516a548d8eb28e10a934b09
Maheera Maryam <110767805+maheera421@users.noreply.github.com>
2023-08-30 17:45:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print("WELCOME TO TIP CALCULATOR!!!\n")
 
 #input from user
 bill= input("What is the total bill? $")
-tip= input("What percentage tip would you like to give? 10, 12 or 15? ")
+tip= input("What percentage tip would you like to give? 10%, 12% or 15%? ")
 ppl= input("How many people to split the bill between? ")
 
 #changing datatypes
